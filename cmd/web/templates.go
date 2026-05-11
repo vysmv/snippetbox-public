35429dd80228eefda898df7283f2a9bd3959006a
vysmv <vysmv.contact@gmail.com>
2026-05-11 12:55:17 +0300

    chapter 5.6

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"html/template" // New import
-	"path/filepath" // New import
+	"html/template"
+	"path/filepath"
+    "time" // New import
 
 	"github.com/vysmv/snippetbox-public/internal/models"
 )
@@ -12,6 +13,19 @@ type templateData struct {
 	CurrentYear int
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
+}
+
+// Initialize a template.FuncMap value and store it in a global variable. This is
+// essentially a string-keyed map which acts as a lookup table mapping names to
+// functions.
+var functions = template.FuncMap{
+    "humanDate": humanDate,
+}
+
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time value.
+func humanDate(t time.Time) string {
+    return t.Format("02 Jan 2006 at 15:04")
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -25,8 +39,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		// The template.FuncMap must be registered with the template set before you
+        // call the ParseFiles() method. This means we have to use template.New() to
+        // create an empty template set, use the Funcs() method to register the
+        // template.FuncMap, and then parse the file as normal.
+        ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
