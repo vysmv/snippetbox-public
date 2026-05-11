f7aeb5ddc43a8af46577e882fe68d198f07fc742
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:42:55 +0300

    chapter 3.2

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,42 +1,38 @@
 package main
 
 import (
-	"flag" // New import
-    "log"
+    "flag"
+    "log/slog" // New import
     "net/http"
+    "os" // New import
 )
 
 func main() {
-    // Define a new command-line flag with the name 'addr', a default value of ":4000"
-    // and some short help text explaining what the flag controls. The value of the
-    // flag will be stored in the addr variable at runtime.
     addr := flag.String("addr", ":4000", "HTTP network address")
-
-    // Importantly, we use the flag.Parse() function to parse the command-line flag.
-    // This reads in the command-line flag value and assigns it to the addr
-    // variable. You need to call this *before* you use the addr variable
-    // otherwise it will always contain the default value of ":4000". If any errors are
-    // encountered during parsing the application will be terminated.
     flag.Parse()
+
+    // Use the slog.New() function to initialize a new structured logger, which
+    // writes to the standard out stream and uses the default settings.
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-    
+
     mux.HandleFunc("GET /{$}", home)
     mux.HandleFunc("GET /snippet/view/{id}", snippetView)
     mux.HandleFunc("GET /snippet/create", snippetCreate)
     mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    // The value returned from the flag.String() function is a pointer to the flag
-    // value, not the value itself. So in this code, that means the addr variable 
-    // is actually a pointer, and we need to dereference it (i.e. prefix it with
-    // the * symbol) before using it. Note that we're using the log.Printf() 
-    // function to interpolate the address with the log message.
-    log.Printf("starting server on %s", *addr)
+    // Use the Info() method to log the starting server message at Info severity
+    // (along with the listen address as an attribute).
+    logger.Info("starting server", "addr", *addr)
 
-    // And we pass the dereferenced addr pointer to http.ListenAndServe() too.
     err := http.ListenAndServe(*addr, mux)
-    log.Fatal(err)
+    // And we also use the Error() method to log any error message returned by
+    // http.ListenAndServe() at Error severity (with no additional attributes),
+    // and then call os.Exit(1) to terminate the application with exit code 1.
+    logger.Error(err.Error())
+    os.Exit(1)
 }
