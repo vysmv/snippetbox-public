55d1a0452f885c6d75e5d1d34dcb36216988777b
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:28:37 +0300

    chapter 2.8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,22 +11,21 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
 
-    // Use the template.ParseFiles() function to read the template file into a
-    // template set. If there's an error, we log the detailed error message, use
-    // the http.Error() function to send an Internal Server Error response to the
-    // user, and then return from the handler so no subsequent code is executed.
-    ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+    // Include the navigation partial in the template files.
+    files := []string{
+        "./ui/html/base.tmpl",
+        "./ui/html/partials/nav.tmpl",
+        "./ui/html/pages/home.tmpl",
+    }
+
+    ts, err := template.ParseFiles(files...)
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
         return
     }
 
-    // Then we use the Execute() method on the template set to write the
-    // template content as the response body. The last parameter to Execute()
-    // represents any dynamic data that we want to pass in, which for now we'll
-    // leave as nil.
-    err = ts.Execute(w, nil)
+    err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
         log.Print(err.Error())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
