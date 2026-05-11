b7ad5f2c9c4d4f7f85f1bb77c55a485db6691f71
vysmv <vysmv.contact@gmail.com>
2026-05-11 13:04:00 +0300

    chapter 6.5

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,18 +1,26 @@
 package main
 
-import "net/http"
+import (
+    "github.com/justinas/alice" // New import
+
+    "net/http"
+)
 
 func (app *application) routes() http.Handler {
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
+   
     mux.HandleFunc("GET /{$}", app.home)
     mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
     mux.HandleFunc("GET /snippet/create", app.snippetCreate)
     mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-    // Wrap the existing chain with the recoverPanic middleware.
-    return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+    // Create a middleware chain containing our 'standard' middleware
+    // which will be used for every request our application receives.
+    standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+    // Return the 'standard' middleware chain followed by the servemux.
+    return standard.Then(mux)
 }
