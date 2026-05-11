b60fcc162b1bb472c88e50796f39e258657d7ac9
vysmv <vysmv.contact@gmail.com>
2026-05-11 14:18:09 +0300

    chapter 10.4

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,10 +7,11 @@ import (
     "unicode/utf8"
 )
 
-// Define a new Validator struct which contains a map of validation error messages 
-// for our form fields.
+// Add a new NonFieldErrors []string field to the struct, which we will use to 
+// hold any validation errors which are not related to a specific form field.
 type Validator struct {
-    FieldErrors map[string]string
+    NonFieldErrors []string
+    FieldErrors    map[string]string
 }
 
 // Use the regexp.MustCompile() function to parse a regular expression pattern
@@ -20,9 +21,16 @@ type Validator struct {
 // variable is more performant than re-parsing the pattern each time we need it.
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-// Valid() returns true if the FieldErrors map doesn't contain any entries.
+// Update the Valid() method to also check that the NonFieldErrors slice is
+// empty.
 func (v *Validator) Valid() bool {
-    return len(v.FieldErrors) == 0
+    return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+// Create an AddNonFieldError() helper for adding error messages to the new
+// NonFieldErrors slice.
+func (v *Validator) AddNonFieldError(message string) {
+    v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 // AddFieldError() adds an error message to the FieldErrors map (so long as no
