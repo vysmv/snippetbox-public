f542336321543bfccb98a626def1f623a0be5737
vysmv <vysmv.contact@gmail.com>
2026-05-11 12:27:06 +0300

    chapter 4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql" // New import
     "flag"
-    "log/slog" // New import
+    "log/slog"
     "net/http"
-    "os" // New import
+    "os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 // Define an application struct to hold the application-wide dependencies for the
@@ -16,9 +19,24 @@ type application struct {
 
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command-line flag for the MySQL DSN string.
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
     flag.Parse()
 
     logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// To keep the main() function tidy I've put the code for creating a connection
+    // pool into the separate openDB() function below. We pass openDB() the DSN
+    // from the command-line flag.
+    db, err := openDB(*dsn)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+
+    // We also defer a call to db.Close(), so that the connection pool is closed
+    // before the main() function exits.
+    defer db.Close()
 
     app := &application{
         logger: logger,
@@ -28,7 +46,24 @@ func main() {
     
     // Call the new app.routes() method to get the servemux containing our routes,
     // and pass that to http.ListenAndServe().
-    err := http.ListenAndServe(*addr, app.routes())
+    err = http.ListenAndServe(*addr, app.routes())
     logger.Error(err.Error())
     os.Exit(1)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/vysmv/snippetbox-public
 
 go 1.25.4
 
-require (
-	filippo.io/edwards25519 v1.2.0 // indirect
-	github.com/go-sql-driver/mysql v1.10.0 // indirect
-)
+require github.com/go-sql-driver/mysql v1.10.0
+
+require filippo.io/edwards25519 v1.2.0 // indirect
