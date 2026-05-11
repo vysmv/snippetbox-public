0fa76018972f2f1c201cdb584114364cae1f0e88
vysmv <vysmv.contact@gmail.com>
2026-05-11 12:59:42 +0300

    chapter 6.3

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,21 +2,17 @@ package main
 
 import "net/http"
 
-// Update the signature for the routes() method so that it returns a
-// http.Handler instead of *http.ServeMux.
 func (app *application) routes() http.Handler {
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-    
+
     mux.HandleFunc("GET /{$}", app.home)
     mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
     mux.HandleFunc("GET /snippet/create", app.snippetCreate)
     mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-    // Pass the servemux as the 'next' parameter to the commonHeaders middleware.
-    // Because commonHeaders is just a function, and the function returns a
-    // http.Handler we don't need to do anything else.
-    return commonHeaders(mux)
+    // Wrap the existing chain with the logRequest middleware.
+    return app.logRequest(commonHeaders(mux))
 }
