d7fb35356d7bf116708613168c29cba73b864350
vysmv <vysmv.contact@gmail.com>
2026-05-11 12:29:31 +0300

    chapter 4.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"database/sql" // New import
-    "flag"
-    "log/slog"
-    "net/http"
-    "os"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+
+	// Import the models package that we just created. You need to prefix this with
+	// whatever module path you set up back in chapter 02.01 (Project Setup and Creating
+	// a Module) so that the import statement looks like this:
+	// "{your-module-path}/internal/models". If you can't remember what module path you
+	// used, you can find it at the top of the go.mod file.
+	"github.com/vysmv/snippetbox-public/internal/models"
 
 	_ "github.com/go-sql-driver/mysql" // New import
 )
@@ -14,56 +21,58 @@ import (
 // web application. For now we'll only include the structured logger, but we'll
 // add more to this as development progresses.
 type application struct {
-    logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
-    addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	// Define a new command-line flag for the MySQL DSN string.
-    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
-    flag.Parse()
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
 
-    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// To keep the main() function tidy I've put the code for creating a connection
-    // pool into the separate openDB() function below. We pass openDB() the DSN
-    // from the command-line flag.
-    db, err := openDB(*dsn)
-    if err != nil {
-        logger.Error(err.Error())
-        os.Exit(1)
-    }
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
-    // We also defer a call to db.Close(), so that the connection pool is closed
-    // before the main() function exits.
-    defer db.Close()
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
-    app := &application{
-        logger: logger,
-    }
+	app := &application{
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
+	}
 
-    logger.Info("starting server", "addr", *addr)
-    
-    // Call the new app.routes() method to get the servemux containing our routes,
-    // and pass that to http.ListenAndServe().
-    err = http.ListenAndServe(*addr, app.routes())
-    logger.Error(err.Error())
-    os.Exit(1)
+	logger.Info("starting server", "addr", *addr)
+
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe().
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
 // The openDB() function wraps sql.Open() and returns a sql.DB connection pool
 // for a given DSN.
 func openDB(dsn string) (*sql.DB, error) {
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        return nil, err
-    }
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
 
-    err = db.Ping()
-    if err != nil {
-        db.Close()
-        return nil, err
-    }
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
 
-    return db, nil
+	return db, nil
 }
