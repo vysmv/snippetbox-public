fd46009733d43d765fb41c3d7f31d8fb5b3acedb
vysmv <vysmv.contact@gmail.com>
2026-05-11 11:48:33 +0300

    chapter 3.3

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,15 +3,15 @@ package main
 import (
     "fmt"
     "html/template" // New import
-    "log"           // New import
     "net/http"
     "strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the home handler so it is defined as a method against
+// *application.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Server", "Go")
 
-    // Include the navigation partial in the template files.
     files := []string{
         "./ui/html/base.tmpl",
         "./ui/html/partials/nav.tmpl",
@@ -20,19 +20,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 
     ts, err := template.ParseFiles(files...)
     if err != nil {
-        log.Print(err.Error())
+        // Because the home handler is now a method against the application
+        // struct it can access its fields, including the structured logger. We'll 
+        // use this to create a log entry at Error level containing the error
+        // message, also including the request method and URI as attributes to 
+        // assist with debugging.
+        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
         return
     }
 
     err = ts.ExecuteTemplate(w, "base", nil)
     if err != nil {
-        log.Print(err.Error())
+        // And we also need to update the code here to use the structured logger
+        // too.
+        app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
         http.Error(w, "Internal Server Error", http.StatusInternalServerError)
     }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetView handler so it is defined as a method
+// against *application.
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.PathValue("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
@@ -42,11 +51,15 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetCreate handler so it is defined as a method
+// against *application.
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+// Change the signature of the snippetCreatePost handler so it is defined as a method
+// against *application.
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusCreated)
     w.Write([]byte("Save a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,32 +7,41 @@ import (
     "os" // New import
 )
 
+// Define an application struct to hold the application-wide dependencies for the
+// web application. For now we'll only include the structured logger, but we'll
+// add more to this as development progresses.
+type application struct {
+    logger *slog.Logger
+}
+
 func main() {
     addr := flag.String("addr", ":4000", "HTTP network address")
     flag.Parse()
 
-    // Use the slog.New() function to initialize a new structured logger, which
-    // writes to the standard out stream and uses the default settings.
     logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+    // Initialize a new instance of our application struct, containing the
+    // dependencies (for now, just the structured logger).
+    app := &application{
+        logger: logger,
+    }
 
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
     mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+    
+    // Swap the route declarations to use the application struct's methods as the
+    // handler functions.
+    mux.HandleFunc("GET /{$}", app.home)
+    mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+    mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+    mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+    
 
-    mux.HandleFunc("GET /{$}", home)
-    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
-    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-
-    // Use the Info() method to log the starting server message at Info severity
-    // (along with the listen address as an attribute).
     logger.Info("starting server", "addr", *addr)
-
+    
     err := http.ListenAndServe(*addr, mux)
-    // And we also use the Error() method to log any error message returned by
-    // http.ListenAndServe() at Error severity (with no additional attributes),
-    // and then call os.Exit(1) to terminate the application with exit code 1.
     logger.Error(err.Error())
     os.Exit(1)
 }
